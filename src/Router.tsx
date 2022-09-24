737223ae0e6232c3d895d1481c9b6c8d2e170520
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-09-24 14:03:08 -0700

    Core Router Created

---
+++ src/Router.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+import LandingPage from "./pages/Landing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  }
+]);
+
+const Router = () => {
+
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default Router;
+
